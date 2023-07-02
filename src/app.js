1ff321483570f283404c3fa6577593ab43efe4af
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-02 18:14:32 -0300

    Utilizando try catch

--- src/app.js
+++ src/app.js
@@ -31,42 +31,31 @@ app.get("/participants", (req,res) => {
 });
 
 //Post participants
-app.post("/participants", (req,res) => {
+app.post("/participants", async(req,res) => {
     const {name} = req.body;
-    db.collection("participants").findOne({name: name})
-        .then( exists => {
-            if(exists){
-                return res.sendStatus(409)
-            }
-            const novoParticipant = {
-                name: name,
-                lastStatus: Date.now()
-            }
-            db.collection("participants").insertOne(novoParticipant)
-                .then(() => {
-                    const novoParticipantMsg = {
-                        from: name,
-                        to: 'Todos',
-                        text: 'entra na sala...',
-                        type: 'status',
-                        time: dayjs().format('HH:mm:ss')
-                    }
-                    db.collection("messages").insertOne(novoParticipantMsg)
-                        .then(() => {
-                            return res.sendStatus(201)
-                        })
-                        .catch(err => {
-                            return res.status(500).send(err.message)
-                        })
-                })
-                .catch(err => {
-                    return res.status(500).send(err.message)
-                })
-
-        })
-        .catch(err => {
-            return res.status(500).send(err.message)
-        })
+    try {
+        const participant = await db.collection("participants").findOne({name: name})
+        if(participant) return res.sendStatus(409)
+        
+        const novoParticipant = {
+            name: name,
+            lastStatus: Date.now()
+        }
+        
+        await db.collection("participants").insertOne(novoParticipant)
+        
+        const novoParticipantMsg = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        }
+        await db.collection("messages").insertOne(novoParticipantMsg)    
+        return res.sendStatus(201)        
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
 })
 
 const PORT = 5000;
